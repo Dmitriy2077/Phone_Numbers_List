fb88b8b1e3a5793b509fb6db24a2a31da3c9eeb4
Lubinskiy Dmitriy <dimka00788@gmail.com>
2022-06-27 20:08:33 +0300

    Added function"dial()". Changed colors.xml.

--- app/src/main/java/mingaz/lubinskiy/app/employee/EmployeeInfoActivity.kt
+++ app/src/main/java/mingaz/lubinskiy/app/employee/EmployeeInfoActivity.kt
@@ -1,10 +1,13 @@
 package mingaz.lubinskiy.app.employee
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import mingaz.lubinskiy.app.R
-import mingaz.lubinskiy.app.department.DEPARTMENT_NAME
 
 const val EMPLOYEE_NAME = "employeeName"
 
@@ -16,5 +19,12 @@ class EmployeeInfoActivity : AppCompatActivity() {
         val bundle: Bundle = intent.extras!!
         val employeeName = bundle.get(EMPLOYEE_NAME).toString()
         toolbar?.title = employeeName
+    }
+
+    fun dialCall(v: View?){
+        val intent = Intent(Intent.ACTION_DIAL)
+        intent.data = Uri.parse("tel:+375001234567")
+        //intent.data = Uri.parse(v?.text.toString())
+        startActivity(intent)
     }
 }
